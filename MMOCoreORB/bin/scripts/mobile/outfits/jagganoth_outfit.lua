7614b0a385625b997a3e7e7525ab0377b00e4bd5
Zac Jones <zjones89@outlook.com>
2023-01-14 00:04:03 -0600

    [Added] WF outfits

---
+++ MMOCoreORB/bin/scripts/mobile/outfits/jagganoth_outfit.lua
@@ -0,0 +1,17 @@
+jagganoth_outfit = {
+	{
+		{objectTemplate = "object/tangible/wearables/vest/vest_s15.iff", customizationVariables = {{"/private/index_color_2", 1}, {"/private/index_color_5", 1}} },
+		{objectTemplate = "object/tangible/wearables/wookiee/wke_hood_s02.iff", customizationVariables = {{"/private/index_color_2", 1}, {"/private/index_color_5", 1}} },
+		{objectTemplate = "object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_leggings.iff", customizationVariables = {{"/private/index_color_2", 1}, {"/private/index_color_1", 1}} },
+		{objectTemplate = "object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_boots.iff", customizationVariables = {{"/private/index_color_2", 1}, {"/private/index_color_1", 74}} },
+		{objectTemplate = "object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_gloves.iff", customizationVariables = {{"/private/index_color_2", 1}, {"/private/index_color_1", 200}} },
+		{objectTemplate = "object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_bracer_l.iff", customizationVariables = {{"/private/index_color_2", 1}, {"/private/index_color_1", 253}} },
+		{objectTemplate = "object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_bracer_r.iff", customizationVariables = {{"/private/index_color_2", 1}, {"/private/index_color_1", 50}} },
+		{objectTemplate = "object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_bicep_l.iff", customizationVariables = {{"/private/index_color_2", 1}, {"/private/index_color_1", 100}} },
+		{objectTemplate = "object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_bicep_r.iff", customizationVariables = {{"/private/index_color_2", 1}, {"/private/index_color_1", 156}} },
+		{objectTemplate = "object/tangible/tcg/series8/wearable_sith_cape.iff", customizationVariables = {} },
+	}
+
+}
+
+addOutfitGroup("jagganoth_outfit", jagganoth_outfit)
