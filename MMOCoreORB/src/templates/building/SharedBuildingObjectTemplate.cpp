4866b8943d47c8ccdf009e6e616af503d127e047
Zac Jones <zjones89@outlook.com>
2023-01-15 03:14:44 -0600

    [Added] Better error printing

--- MMOCoreORB/src/templates/building/SharedBuildingObjectTemplate.cpp
+++ MMOCoreORB/src/templates/building/SharedBuildingObjectTemplate.cpp
@@ -159,7 +159,11 @@ void SharedBuildingObjectTemplate::readObject(IffStream* iffStream) {
 		parseFileData(iffStream);
 	} catch (Exception& e) {
 		String msg;
-		msg += "exception caught parsing file data ->";
+
+		String fName = iffStream->getFileName();
+
+		msg += "exception in " + fName + ": caught parsing file data ->";
+		
 		msg += e.getMessage();
 
 		Logger::console.error(msg);
